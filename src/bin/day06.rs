287e3e4d0442a18c102bad72adca4f7e8c6e25bd
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-07 16:30:03 +0100

    Day 6: Part 2

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -5,11 +5,28 @@ use aoc2019::read_lines;
 
 fn main() {
     let lines: Vec<String> = read_lines(6);
-    println!("pt1: {}", checksum(&orbitalMap(&lines))); // 224901
+    let orbits = orbitalMap(&lines);
+    println!("pt1: {}", checksum(&orbits)); // 224901
+    println!("pt2: {}", path_between(String::from("YOU"),String::from("SAN"),&orbits)); // 334
 }
 
+fn path_to_COM(origin: String, orbits: &HashMap<String, String>) -> Vec<String> {
+    successors(orbits.get(&origin), |name| orbits.get(*name)).cloned().collect()
+}
 
-fn checksum(orbits: &HashMap<String, String>) -> u32{
+fn path_between(origin: String, target: String, orbits: &HashMap<String, String>) -> usize {
+    let origin_path = path_to_COM(origin, orbits);
+    let target_path = path_to_COM(target, orbits);
+
+    let intersection : &String = origin_path.iter()
+        .filter(|p| target_path.contains(p))
+        .next().unwrap();
+    let origin_distance = origin_path.iter().position(|other| other == intersection);
+    let target_distance = target_path.iter().position(|other| other == intersection);
+    [origin_distance, target_distance].into_iter().flatten().sum()
+}
+
+fn checksum(orbits: &HashMap<String, String>) -> u32 {
     orbits.keys()
         .into_iter()
         .map(|elem|  successors(orbits.get(elem), |name| orbits.get(*name)).count() as u32)
@@ -48,5 +65,31 @@ mod test {
         assert_eq!(orbits["D"], "C");
 
         assert_eq!(checksum(&orbits), 42);
+    }
+    #[test]
+    fn test_ex_pt2() {
+        let example: Vec<String> = vec![
+            "COM)B",
+            "B)C",
+            "C)D",
+            "D)E",
+            "E)F",
+            "B)G",
+            "G)H",
+            "D)I",
+            "E)J",
+            "J)K",
+            "K)L",
+            "K)YOU",
+            "I)SAN"].into_iter().map(String::from).collect();
+        let orbits = orbitalMap(&example);
+        assert_eq!(orbits["B"], "COM");
+        assert_eq!(orbits["C"], "B");
+        assert_eq!(orbits["D"], "C");
+
+        assert_eq!(path_to_COM(String::from("YOU"), &orbits), vec!["K","J","E","D","C","B","COM"]);
+        assert_eq!(path_to_COM(String::from("SAN"), &orbits), vec!["I","D","C","B","COM"]);
+
+        assert_eq!(path_between(String::from("YOU"),String::from("SAN"), &orbits), 4)
     }
 }
