6c5449676a0d53a303eece7fabfad1754e2662f6
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-07 11:21:20 +0100

    Day 5: Part 2 refactoring

--- src/bin/day05.rs
+++ src/bin/day05.rs
@@ -1,11 +1,11 @@
 use aoc2019::read_lines;
 use std::cmp::{max, min};
+use std::borrow::Borrow;
 
 fn main() {
     let nums: Vec<i32> = read_lines(5)[0].split(',').map(|s| s.parse().unwrap()).collect();
     println!("pt1: {}", IntCode::resolve(1, &nums)); // 16434972
-    println!("pt2: {}", IntCode::resolve(5, &nums)); //
-    //println!("pt2: {}", pt2(&nums)); // 2552
+    println!("pt2: {}", IntCode::resolve(5, &nums)); // 16694270
 }
 
 impl IntCode {
@@ -21,63 +21,64 @@ impl IntCode {
     fn run(&mut self) -> i32{
         let scope = &mut self.pgm;
         while self.csr.is_some() {
-            self.run_single();
+            self.csr = self.run_single();
         }
         *self.output.last().unwrap_or(&0)
     }
-    fn run_single(&mut self) {
+    fn run_single(&mut self) -> Option<usize> {
         let csr = self.csr.unwrap();
-        let scope = &mut self.pgm;
-        let (op, param_modes) = parse_op(scope[csr] as u32);
-        let curr_view:Vec<i32> = scope.clone().into_iter().skip(csr).take(min(4, scope.len())).collect();
-        println!("{:?}", curr_view);
+        let (op, param_modes) = parse_op(self.pgm[csr] as u32);
+        //let curr_view:Vec<i32> = scope.clone().into_iter().skip(csr).take(min(4, scope.len())).collect();
+        //println!("{:?}", curr_view);
+        let(p1,p2) = self.params(&param_modes);
         if op == 1 { // add
-            let p1 = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            let p2 = resolve_param(param_modes[1], scope[csr + 2], &scope);
-            let out_csr = scope[csr + 3] as usize;
-            scope[out_csr] = p1 + p2;
-            self.csr = Some(csr + 4);
+            let out_csr = self.pgm[csr + 3] as usize;
+            self.pgm[out_csr] = p1? + p2?;
+            Some(csr + 4)
         } else if op == 2 { // mul
-            let p1 = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            let p2 = resolve_param(param_modes[1], scope[csr + 2], &scope);
-            let out_csr = scope[csr + 3] as usize;
-            scope[out_csr] = p1 * p2;
-            self.csr = Some(csr + 4)
+            let out_csr = self.pgm[csr + 3] as usize;
+            self.pgm[out_csr] = p1? * p2?;
+            Some(csr + 4)
         } else if op == 3 { // read_in
-            let out_csr = scope[csr + 1] as usize;
-            scope[out_csr] = self.input as i32;
-            self.csr = Some(csr + 2)
+            let out_csr = self.pgm[csr + 1] as usize;
+            self.pgm[out_csr] = self.input as i32;
+            Some(csr + 2)
         } else if op == 4 { //write_out
-            let out = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            self.output.push(out);
-            println!("-> {}",out);
-            self.csr = Some(csr + 2)
+            self.output.push(p1?);
+            Some(csr + 2)
         } else if op == 5 { // jump-if-true
-            let p1 = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            let p2 = resolve_param(param_modes[1], scope[csr + 2], &scope);
-            self.csr = if p1 != 0 { Some(p2 as usize) } else { Some(csr + 3) }
+            if p1? != 0 { Some(p2? as usize) } else { Some(csr + 3) }
         } else if op == 6 { // jump-if-false
-            let p1 = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            let p2 = resolve_param(param_modes[1], scope[csr + 2], &scope);
-            self.csr = if p1 == 0 { Some(p2 as usize) } else { Some(csr + 3) }
+            if p1? == 0 { Some(p2? as usize) } else { Some(csr + 3) }
         } else if op == 7 { // less-than
-            let p1 = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            let p2 = resolve_param(param_modes[1], scope[csr + 2], &scope);
-            let out_csr = scope[csr + 3] as usize;
-            scope[out_csr] = (p1 < p2) as i32;
-            self.csr = Some(csr + 4)
+            let out_csr = self.pgm[csr + 3] as usize;
+            self.pgm[out_csr] = (p1? < p2?) as i32;
+            Some(csr + 4)
         } else if op == 8 { // less-than
-            let p1 = resolve_param(param_modes[0], scope[csr + 1], &scope);
-            let p2 = resolve_param(param_modes[1], scope[csr + 2], &scope);
-            let out_csr = scope[csr + 3] as usize;
-            scope[out_csr] = (p1 == p2) as i32;
-            self.csr = Some(csr + 4)
-        } else if op == 99{
-            println!("halt at csr: {}", csr);
-            self.csr = None;
+            let out_csr = self.pgm[csr + 3] as usize;
+            self.pgm[out_csr] = (p1? == p2?) as i32;
+            Some(csr + 4)
+        } else if op == 99 {
+            None
         } else {
-            panic!("problem!"); // or is it?! exiting after an output is ok (via instructions)
+            panic!("Found an unknown opcode");
         }
+    }
+
+    fn params(&self,param_modes: &Vec<u32>) -> (Option<i32>, Option<i32>){
+        let csr = self.csr.unwrap();
+        let p1 = self.pgm.get(csr + 1).and_then(|p_imm| resolve_param(param_modes[0],*p_imm,&self.pgm));
+        let p2 = self.pgm.get(csr + 2).and_then(|p_imm| resolve_param(param_modes[1],*p_imm,&self.pgm));
+        (p1,p2)
+    }
+}
+fn resolve_param(mode : u32, param: i32, scope: &Vec<i32>) -> Option<i32> {
+    if mode == 0 {
+        scope.get(param as usize).cloned()
+    } else if mode == 1 {
+        Some(param)
+    } else {
+        panic!("ParamMode not defined!")
     }
 }
 
@@ -95,15 +96,6 @@ fn parse_op(input: u32) -> (u32, Vec<u32>) {
     (op, param_modes)
 }
 
-fn resolve_param(mode : u32, param: i32, scope: &Vec<i32>) -> i32 {
-    if mode == 0 {
-        scope[param as usize]
-    } else if mode == 1 {
-        param
-    } else {
-        panic!("ParamMode not defined!")
-    }
-}
 
 #[cfg(test)]
 mod test {
@@ -114,28 +106,28 @@ mod test {
         assert_eq!(parse_op(1002), (2,vec![0,1,0]));
         assert_eq!(parse_op(1102), (2, vec![1,1,0]));
         assert_eq!(parse_op(10002), (2, vec![0,0,1]));
-        assert_eq!(resolve_param(0,0, &vec![99]), 99);
-        assert_eq!(resolve_param(1,0, &vec![99]), 0);
+        assert_eq!(resolve_param(0,0, &vec![99]), Some(99));
+        assert_eq!(resolve_param(1,0, &vec![99]), Some(0));
     }
     #[test]
     fn test_single_instr() {
         let mut scope = vec![1,0,0,0,99];
         let mut toTest = IntCode::create(0,&vec![1,0,0,0,99]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.csr, Some(4));
         assert_eq!(toTest.pgm, vec![2,0,0,0,99]);
         toTest.csr_at(0);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.csr, Some(4));
         assert_eq!(toTest.pgm, vec![4,0,0,0,99]);
 
         toTest = IntCode::create(0,&vec![1002,4,3,4,33]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.csr, Some(4));
         assert_eq!(toTest.pgm, vec![1002,4,3,4,99]);
 
         toTest = IntCode::create(0,&vec![1101,100,-1,4,0]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.csr, Some(4));
         assert_eq!(toTest.pgm, vec![1101,100,-1,4,99]);
     }
@@ -159,13 +151,13 @@ mod test {
     #[test]
     fn fix_nonzero_code() {
         let mut toTest = IntCode::create(1, &vec![3, 15, 1, 15, 6, 6, 1100, 1, 238, 15, 104, 0, 1101, 40, 0, /*15*/0]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.pgm, vec![3,15,1,15,6,6,1100,1,238,15,104,0,1101,40,0,/*15*/1]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.pgm, vec![3,15,1,15,6,6,1101,1,238,15,104,0,1101,40,0,/*15*/1]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.pgm, vec![3,15,1,15,6,6,1101,1,238,15,104,0,1101,40,0,/*15*/239]);
-        toTest.run_single();
+        toTest.csr = toTest.run_single();
         assert_eq!(toTest.pgm, vec![3,15,1,15,6,6,1101,1,238,15,104,0,1101,40,0,/*15*/239]);
     }
 
