5a0fb6da904c217b9a16092eb34f58903e1a7e42
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-09 15:17:16 +0100

    Day9: Part 1

---
+++ src/bin/day09.rs
@@ -0,0 +1,8 @@
+use aoc2019::read_lines;
+use aoc2019::intcode::IntCode;
+
+fn main() {
+    let nums: Vec<i64> = read_lines(9)[0].split(',').map(|s| s.parse().unwrap()).collect();
+    println!("pt1: {}", IntCode::resolve(&vec![1], &nums)); // 4261108180
+    //println!("pt2: {}", IntCode::resolve(&vec![5], &nums)); //
+}
