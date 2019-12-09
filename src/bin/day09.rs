dbb6f8ff8618fce41281d6c79271d369c80146af
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-09 23:58:19 +0100

    Add regression test for day 9 - there might be some more IntCode puzzles

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -6,3 +6,16 @@ fn main() {
     println!("pt1: {}", IntCode::resolve(&vec![1], &nums)); // 4261108180
     println!("pt2: {}", IntCode::resolve(&vec![2], &nums)); // 77944
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn regression() {
+        let nums: Vec<i64> = read_lines(9)[0].split(',').map(|s| s.parse().unwrap()).collect();
+        assert_eq!(IntCode::resolve(&vec![1], &nums), 4261108180);
+        assert_eq!(IntCode::resolve(&vec![2], &nums), 77944);
+    }
+}
