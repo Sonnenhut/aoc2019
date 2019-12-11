857a452e4c284a473949ba3c4a403434f1aefa74
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-11 15:11:47 +0100

    Replacing nested for loops with something different (is it really more readable?!)

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -18,18 +18,29 @@ fn pt1(mem: &Vec<i64>) -> i64 {
 }
 
 fn pt2(mem: &Vec<i64>) -> i64 {
-    for noun in 0..100 {
-        for verb in 0..100 {
+    (0..100).flat_map(|n| (0..100).map(move |v| (n,v)))
+        .map(|(noun, verb)| {
             let mut modified_mem = mem.to_vec();
             modified_mem[1] = noun;
             modified_mem[2] = verb;
             let mut int_code = IntCode::create(&vec![], &modified_mem);
             int_code.run();
-            let res = int_code.memory()[0];
-            if res == 19690720 {
-                return 100 * noun + verb;
-            }
-        }
+            ( int_code.memory()[0], 100 * noun + verb )
+        })
+        .filter(|(out, _)| *out == 19690720)
+        .map(|(_, res)| res)
+        .next().unwrap()
+}
+
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn regression() {
+        let mem: Vec<i64> = read_lines(2)[0].split(',').map(|s| s.parse().unwrap()).collect();
+        assert_eq!(pt1(&mem), 9706670);
+        assert_eq!(pt2(&mem), 2552);
     }
-    0
 }
