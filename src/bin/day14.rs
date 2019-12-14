4fac26cc53a74904f4fcebe1b7b36e3a5d8a8787
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-14 19:26:15 +0100

    Day 14: Part 1 - not proud at all

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -6,30 +6,27 @@ use std::hash::Hash;
 use std::iter::successors;
 
 fn main() {
-    //let mem: Vec<i64> = read_lines(14)[0].split(',').map(|s| s.parse().unwrap()).collect();
-    //println!("pt1: {}", pt1(&mem)); //
+
+    let reactions = parse(read_lines(14));
+    println!("pt1: {}", pt1(&reactions)); // 337075
     //println!("pt2: {}", pt2(&mem, true)); //
 }
 
-
-fn pt1(reactions: HashMap<String, Vec<Chem>>) -> usize{
-0
+fn pt1(reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
+    explode(Chem::new(1,&"FUEL".to_string()), &reactions)
 }
 
-
-fn explode(to_explode: &Vec<Chem>, reactions: &HashMap<Chem, Vec<Chem>>) {
+fn explode(c: Chem, reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
     let mut rest : HashMap<String, i64> = HashMap::new();
 
-    let mut res : Vec<Chem> = to_explode.to_vec();
+    let mut res : Vec<Chem> = vec![c];
     let mut i = 0;
-    while res.len() > 1 {
+    while !(res[0].name == String::from("ORE") && res.len() == 1) {
         res = res.iter_mut()
             .flat_map(|chem| {
                 let existing_rest = *rest.get(&chem.name).unwrap_or(&0);
                 if existing_rest > 0 {
-                    println!("chem: {:?} rest exists{:?}", chem, existing_rest);
-                    let sub = (chem.amount - existing_rest.max(chem.amount));
-
+                    let sub = existing_rest.min(chem.amount);
                     chem.amount -= sub;
                     rest.insert(chem.name.clone(), existing_rest - sub);
                 }
@@ -38,26 +35,20 @@ fn explode(to_explode: &Vec<Chem>, reactions: &HashMap<Chem, Vec<Chem>>) {
                 }
                 return if let Some((chem_yield, needed)) = find_reaction(&chem.name, &reactions) {
                     let amount_to_create = next_highest_multiple(chem.amount, chem_yield.amount);
-                    let modulo = amount_to_create % chem_yield.amount;
+                    let modulo = amount_to_create - chem.amount;
                     let multiply = (amount_to_create) / chem_yield.amount;
                     let xploded : Vec<Chem> = needed.clone().iter_mut().map(|needed_chem| Chem::new(needed_chem.amount * multiply, needed_chem.name.as_ref())).collect();
                     if modulo > 0 {
                         rest.insert(chem.name.clone(), rest.get(&chem.name).unwrap_or(&0) + modulo);
                     }
-                    println!("chem: {:?} reaction into: {:?}; rest: {:?}", chem, xploded, modulo);
                     xploded
                 } else {
                     vec![chem.clone()]
                 }
             }).collect();
         res = flatten(&res);
-        println!("==> {:?}; rest: {:?}", res, rest);
-        i += 1;
-        if i== 10 {
-            break;
-        }
     }
-    //(vec![], vec![])
+    res[0].amount
 }
 
 fn flatten(v: &Vec<Chem>) -> Vec<Chem>{
@@ -138,33 +129,8 @@ mod test {
         expected.insert(Chem:: new(1, "FUEL"), vec![Chem:: new(7, "A"), Chem:: new(1, "E")]);
 
         assert_eq!(parse(ex.lines().map(String::from).collect()), expected);
-
-        //assert_eq!(flatten_ore(&expected), 31);
-/*
-        ex =
-            "9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL";
-        assert_eq!(flatten_ore(&parse(ex.lines().map(String::from).collect())), 165);
-
-        ex =
-"157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
-        assert_eq!(flatten_ore(&parse(ex.lines().map(String::from).collect())), 13312 );
-*/
-        //assert_eq!((23 as f64 / 3 as f64).ceil() as usize, 8);
     }
+
     #[test]
     fn test_flatten() {
         let unflattened  = vec![Chem:: new(1, "A"), Chem:: new(2, "A")];
@@ -179,32 +145,56 @@ mod test {
     }
     #[test]
     fn test_explode() {
-
-        /*
-
-        let ex =
-"9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL";
-        let reactions = parse(ex.lines().map(String::from).collect());
-        explode(&vec![Chem::new(2,"AB"),Chem::new(3,"BC"),Chem::new(4,"CA")], &reactions);
-        */
+        let mut ex =
+            "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
+        let mut reactions = parse(ex.lines().map(String::from).collect());
+        assert_eq!(explode(Chem::new(1,&"FUEL".to_string()), &reactions), 13312);
 
 
-        let ex =
-"9 ORE => 2 A
-8 ORE => 3 B
-7 ORE => 5 C
-3 A, 4 B => 1 AB
-5 B, 7 C => 1 BC
-4 C, 1 A => 1 CA
-2 AB, 3 BC, 4 CA => 1 FUEL";
-        let reactions = parse(ex.lines().map(String::from).collect());
-        explode(&find_reaction(&"FUEL".to_string(), &reactions).unwrap().1, &reactions);
+        ex =
+            "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF";
+        reactions = parse(ex.lines().map(String::from).collect());
+        assert_eq!(explode(Chem::new(1,&"FUEL".to_string()), &reactions), 180697 );
+
+        ex =
+            "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX";
+        reactions = parse(ex.lines().map(String::from).collect());
+        assert_eq!(explode(Chem::new(1,&"FUEL".to_string()), &reactions), 2210736);
 
         //assert_eq!(explode(&Chem::new(2,"AB"), &reactions), vec![Chem::new(6,"A"), Chem::new(8,"B")]);
         //assert_eq!(explode(&Chem::new(3,"A"), &reactions), vec![Chem::new(18,"ORE"), Chem::new(1,"A")]);
