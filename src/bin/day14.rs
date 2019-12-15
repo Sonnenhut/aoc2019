fafe9ac7ddccbf4ec07e3a415611e1cdf2f085dc
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-15 01:00:39 +0100

    Day 14: part 2 "clean up" that's still not clean though

--- src/bin/day14.rs
+++ src/bin/day14.rs
@@ -1,8 +1,5 @@
 use std::collections::HashMap;
-
-
 use aoc2019::read_lines;
-use std::hash::Hash;
 use std::iter::successors;
 use std::cmp::Ordering;
 use std::ops::RangeInclusive;
@@ -14,57 +11,49 @@ fn main() {
 }
 
 fn pt1(reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
-    explode(Chem::new(1,&"FUEL".to_string()), &reactions)
+    explode(Chem::new(1, &"FUEL".to_string()), &reactions)
 }
 
 fn pt2(reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
     let ore_for_one_fuel = pt1(&reactions);
     let trillion = 1000000000000;
-    let possible_fuels: Vec<i64> = (trillion/ore_for_one_fuel..=trillion/ore_for_one_fuel*2).collect();
+    let fuel_range: Vec<i64> = (trillion/ore_for_one_fuel..=trillion/ore_for_one_fuel*2).collect();
 
-    let res = possible_fuels.binary_search_by(|fuel| {
-        let ore_needed = explode(Chem::new(*fuel,&"FUEL".to_string()), &reactions);
+    let res = fuel_range.binary_search_by(|fuel| {
+        let ore_needed = explode(Chem::new(*fuel, &"FUEL".to_string()), &reactions);
         let target_range = (trillion-ore_for_one_fuel..=trillion);
-        if target_range.contains(&ore_needed) {
-            Ordering::Equal
-        } else if ore_needed < *target_range.start() {
-            Ordering::Less
-        } else if *target_range.end() < ore_needed {
-            Ordering::Greater
-        } else {panic!("unexpected idx not covered")}
+        if target_range.contains(&ore_needed) { Ordering::Equal }
+        else if ore_needed < *target_range.start() { Ordering::Less }
+        else if *target_range.end() < ore_needed { Ordering::Greater }
+        else { panic!("Idx not covered in if/else branch") }
     });
-    possible_fuels[res.ok().unwrap_or(0)] as i64
+    fuel_range[res.ok().unwrap_or(0)] as i64
 }
 
-
 fn explode(c: Chem, reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
-    let mut rest : HashMap<String, i64> = HashMap::new();
-
+    let mut dangling: HashMap<String, i64> = HashMap::new();
     let mut res : Vec<Chem> = vec![c];
-    let mut i = 0;
+
     while !(res[0].name == String::from("ORE") && res.len() == 1) {
-        res = res.iter_mut()
-            .flat_map(|chem| {
-                let existing_rest = *rest.get(&chem.name).unwrap_or(&0);
-                if existing_rest > 0 {
-                    let sub = existing_rest.min(chem.amount);
-                    chem.amount -= sub;
-                    rest.insert(chem.name.clone(), existing_rest - sub);
-                }
-                if chem.amount == 0 {
-                    return vec![]
-                }
-                return if let Some((chem_yield, needed)) = find_reaction(&chem.name, &reactions) {
-                    let amount_to_create = next_highest_multiple(chem.amount, chem_yield.amount);
-                    let modulo = amount_to_create - chem.amount;
+        res = res.iter().flat_map(|Chem {name, amount:init_amount}| {
+                let mut amount = *init_amount;
+                let existing_rest = *dangling.get(name).unwrap_or(&0);
+                let sub = existing_rest.min(amount);
+                amount -= sub;
+                dangling.insert(name.clone(), existing_rest - sub);
+                if amount == 0 { return vec![] }
+
+                return if let Some((chem_yield, needed)) = reactions.iter().filter(|(other, _)| &other.name == name).next() {
+                    let amount_to_create = next_highest_multiple(amount, chem_yield.amount);
+                    let modulo = amount_to_create - amount;
                     let multiply = (amount_to_create) / chem_yield.amount;
-                    let xploded : Vec<Chem> = needed.clone().iter_mut().map(|needed_chem| Chem::new(needed_chem.amount * multiply, needed_chem.name.as_ref())).collect();
                     if modulo > 0 {
-                        rest.insert(chem.name.clone(), rest.get(&chem.name).unwrap_or(&0) + modulo);
+                        dangling.insert(name.clone(), dangling.get(name).unwrap_or(&0) + modulo);
                     }
-                    xploded
+                    needed.iter()
+                        .map(|needed_chem| Chem::new(needed_chem.amount * multiply, &needed_chem.name)).collect()
                 } else {
-                    vec![chem.clone()]
+                    vec![Chem::new(amount,name)] // in case of ORE, ignore
                 }
             }).collect();
         res = flatten(&res);
@@ -73,28 +62,18 @@ fn explode(c: Chem, reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
 }
 
 fn flatten(v: &Vec<Chem>) -> Vec<Chem>{
-    v.iter()
-        .fold(vec![],|mut acc,next| {
+    v.iter().fold(vec![],|mut acc,next| {
             if let Some(existing) = acc.iter_mut().filter(|other| other.name == next.name).next() {
                 existing.amount += next.amount;
-            } else {
-                acc.push(next.clone());
-            }
+            } else { acc.push(next.clone()); }
             acc
         })
-}
-fn find_reaction(chem_name: &String, reactions: &HashMap<Chem, Vec<Chem>>) -> Option<(Chem, Vec<Chem>)> {
-    reactions.iter().filter(|(other, _)| other.name == *chem_name)
-        .map(|other| (other.0.clone(), other.1.clone()))
-        .next()
 }
 
 fn parse(input: Vec<String>) -> HashMap<Chem, Vec<Chem>> {
     input.iter().map(|line| {
         let mut left_right : Vec<&str> = line.split(" => ").collect();
-        let needed_chem = parse_chem(&left_right[0].into());
-        let res_chem = parse_chem(&left_right[1].into()).remove(0);
-        (res_chem,needed_chem)
+        (parse_chem(&left_right[1].into()).remove(0), parse_chem(&left_right[0].into()))
     }).collect()
 }
 
@@ -106,26 +85,13 @@ fn parse_chem(halve_line: &String) -> Vec<Chem> {
         }).collect()
 }
 
-fn next_highest_multiple(n: i64, multiple: i64) -> i64 {
-    if multiple == 0 {
-        return n;
-    }
-
-    let remainder = n % multiple;
-    if remainder == 0 {
-        return n;
-    }
-
-    return n + multiple - remainder;
-}
+fn next_highest_multiple(n: i64, multiple: i64) -> i64 { ((n + multiple-1) / multiple) * multiple }
 
 #[derive(Debug, Clone, PartialOrd, PartialEq, Ord, Eq, Hash)]
-struct Chem {amount: i64,name:  String}
+struct Chem {amount: i64, name: String}
 
 impl Chem {
-    fn new(amount: i64, name: &str) -> Chem{
-        Chem {amount, name: name.to_string()}
-    }
+    fn new(amount: i64, name: &str) -> Chem { Chem {amount, name: name.to_string()} }
 }
 
 #[cfg(test)]
@@ -160,10 +126,27 @@ mod test {
     }
     #[test]
     fn test_next_highest_multiple() {
+        assert_eq!(next_highest_multiple(1,4), 4);
+        assert_eq!(next_highest_multiple(4,4), 4);
         assert_eq!(next_highest_multiple(2,4), 4);
         assert_eq!(next_highest_multiple(23,3), 24);
         assert_eq!(next_highest_multiple(15,3), 15);
     }
+
+
+    #[test]
+    fn test_simple_ex() {
+        let mut ex =
+            "10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL";
+        let mut reactions = parse(ex.lines().map(String::from).collect());
+        assert_eq!(pt1(&reactions), 31);
+    }
+
     #[test]
     fn test_parts() {
         let mut ex =
@@ -220,3 +203,49 @@ mod test {
         assert_eq!(pt2(&reactions), 460664  );
     }
 }
+
+/*
+// gave it a try with a hashmap, but I think the order has to be kept
+fn explode(c: Chem, reactions: &HashMap<Chem, Vec<Chem>>) -> i64 {
+    let mut inventory: HashMap<String, i64> = HashMap::new();
+
+    let mut res : HashMap<String, i64> = HashMap::new();
+    res.insert(c.name, c.amount);
+
+    while !(res.len() == 1 && *res.get("ORE").unwrap_or(&0) != 0) {
+        for name in res.clone().keys().map(String::from) {
+            let amount = res[&name];
+            let existing_rest = *inventory.get(&name).unwrap_or(&0);
+            if existing_rest > 0 {
+                let sub = existing_rest.min(amount);
+                res.insert(name.clone(), amount - sub);
+                inventory.insert(name.to_string(), existing_rest - sub);
+                println!("taking rest out of ivnentory {} {}", name, sub);
+                println!("ivnentory is now {:?}", inventory);
+            }
+            if amount == 0 {
+                res.remove(&name);
+                continue;
+            }
+            if let Some((chem_yield, needed)) = find_reaction(&name, &reactions) {
+                let amount_to_create = next_highest_multiple(amount, chem_yield.amount);
+                let modulo = amount_to_create - amount;
+                let multiply = (amount_to_create) / chem_yield.amount;
+                let xploded : Vec<Chem> = needed.clone().iter_mut().map(|needed_chem| Chem::new(needed_chem.amount * multiply, needed_chem.name.as_ref())).collect();
+                for x in xploded {
+                    res.insert(x.name.clone(), res.get(&x.name).unwrap_or(&0) + x.amount);
+                }
+                if modulo > 0 {
+                    inventory.insert(name.clone(), inventory.get(&name).unwrap_or(&0) + modulo);
+                }
+                println!("removing {:?}", name);
+                res.remove(&name);
+            }
+            println!("res {:?}; inventory {:?}", res, inventory);
+        }
+        //println!("res {:?}", res);
+        //break;
+        //res = flatten(&res);
+    }
+    res["ORE"]
+}*/
