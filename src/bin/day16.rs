46c655de9063d1d6da1aeb871bc32688eaaa3b88
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-12-16 10:27:14 +0100

    WIP Day 16

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -3,6 +3,7 @@ use aoc2019::read_lines;
 use aoc2019::intcode::IntCode;
 use std::sync::mpsc::{Sender, Receiver};
 use std::iter::{repeat, Skip, FlatMap};
+use std::time::Instant;
 
 fn main() {
     let input = &read_lines(16)[0];
@@ -48,27 +49,38 @@ fn fft(n: &String, phase_cnt: usize) -> String{
     let base_pattern = [0,1,0,-1].to_vec();
     let mut seq : Vec<i32> = n.chars().map(|c| c.to_digit(10).unwrap() as i32).collect();
 
-    for _ in 0..phase_cnt {
-        let mut out_list= vec![]; //: Vec<i32> = seq.clone();
-
-        for i in 0..seq.len() {
-            let mut out_idx = i;
-            let pattern = base_pattern.iter()
+    let patterns : Vec<Vec<i32>> = (0..seq.len())
+        .map(|i| {
+            base_pattern.iter()
                 .flat_map(|x| {
-                    repeat(*x).take(out_idx + 1).collect::<Vec<i32>>()
+                    repeat(*x).take(i + 1).collect::<Vec<i32>>()
                 })
-                .cycle()
-                .skip(1);
-            let out_raw : i32 = seq.iter().zip(pattern)
+                .collect()
+        }).collect();
+
+
+
+    let mut out_list: Vec<i32> = seq.clone();
+    for _ in 0..phase_cnt {
+        let start = Instant::now();
+        for i in 0..out_list.len() {
+            //let mut pattern = &patterns[i].iter().collect::<Vec<_>>();
+            //println!("seq     is {:?}", out_list);
+            //println!("pattern is {:?}", pattern);
+            let out_raw : i32 = out_list.iter().zip((&patterns[i]).iter().cycle().skip(1))
+                .filter(|(lhs,rhs)| **lhs != 0 && **rhs != 0)
                 .map(|(lhs,rhs)| lhs * rhs)
                 .sum();
             let out = (out_raw % 10).abs();
-            out_list.push(out);//[i] = out;
+            out_list[i] = out;
+            //println!("[{}] is {:?}", i, out);
+            //out_list.push(out);//[i] = out;
         }
+        println!("duration for one loop {:?} for numbers {}", start.elapsed(), out_list.len());
         //println!("phase complete {:?}", out_list);
-        seq = out_list
+        //seq = out_list
     }
-    let str_res : Vec<String> = seq.iter().map(|x| x.to_string()).collect();
+    let str_res : Vec<String> = out_list.iter().map(|x| x.to_string()).collect();
     str_res.concat()
 }
 
@@ -76,6 +88,11 @@ fn fft(n: &String, phase_cnt: usize) -> String{
 #[cfg(test)]
 mod test {
     use super::*;
+    #[test]
+    fn test_regression() {
+        let input = &read_lines(16)[0];
+        assert_eq!(pt1(&input), "22122816");
+    }
     #[test]
     fn test_ex() {
         fft(&"12345678".to_string(),3);
