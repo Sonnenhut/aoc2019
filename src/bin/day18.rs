ad2c0df4d1d18d12f955efd0e03b13fac0bd924a
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-01-24 16:24:50 +0100

    Day 18 WIP

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -11,6 +11,17 @@ fn main() {
     //println!("pt2: {}", pt2(&mem)); // 360
 
     let mut ex =
+        "########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################";
+
+    let mut maze = ex.split('\n').map(String::from).collect();
+    assert_eq!(shortest_path_all_keys(&maze),86);
+
+
+    ex =
         "#################
 #i.G..c...e..H.p#
 ########.########
@@ -21,27 +32,21 @@ fn main() {
 #l.F..d...h..C.m#
 #################";
 
-    let mut maze = ex.split('\n').map(String::from).collect();
-    let start = Instant::now();
-    let res = shortest_path_all_keys(&maze);
-    println!("found {} after {:?}", res, start.elapsed());
-    assert_eq!(res, 132);
+    maze = ex.split('\n').map(String::from).collect();
+    assert_eq!(shortest_path_all_keys(&maze),132);
 }
 
 #[derive(Clone, Eq, PartialEq)]
 struct State {
     steps: usize,
-    position: Coord,
-//    collected_keys: Vec<char>
+    position: Coord
 }
 
 impl Ord for State {
     fn cmp(&self, other: &State) -> Ordering {
         other.steps.cmp(&self.steps)
+            .then_with(|| other.position.collected_keys.len().cmp(&self.position.collected_keys.len()))
             .then_with(|| self.position.cmp(&other.position))
-//        other.collected_keys.len().cmp(&self.collected_keys.len())
-//            .then_with(|| other.steps.cmp(&self.steps))
-//            .then_with(|| self.position.cmp(&other.position))
     }
 }
 
@@ -51,20 +56,20 @@ impl PartialOrd for State {
     }
 }
 
-
 #[derive(Debug, Clone, PartialOrd, PartialEq, Ord, Eq, Hash)]
 struct Coord {
     x: usize,
-    y: usize
+    y: usize,
+    collected_keys: Vec<char>
 }
 
 impl Coord {
     fn shift(&self, d: i64) -> Coord {
         match d {
-            1 => Coord{x:self.x+1, y: self.y},
-            2 => Coord{x:self.x-1, y: self.y},
-            3 => Coord{x:self.x, y: self.y-1},
-            4 => Coord{x:self.x, y: self.y+1},
+            1 => Coord{x:self.x+1, y: self.y, collected_keys: self.collected_keys.clone()},
+            2 => Coord{x:self.x-1, y: self.y, collected_keys: self.collected_keys.clone()},
+            3 => Coord{x:self.x, y: self.y-1, collected_keys: self.collected_keys.clone()},
+            4 => Coord{x:self.x, y: self.y+1, collected_keys: self.collected_keys.clone()},
             _ => panic!("cannot go in given direction (direction unknown)")
         }
     }
@@ -82,7 +87,7 @@ fn walkables(maze: &Vec<String>) -> HashMap<char, Coord> {
         .flat_map(|y|
             (0..maze[y].len())
                 .filter_map(|x| {
-                    let coord = Coord{x,y};
+                    let coord = Coord{x,y, collected_keys: vec![]};
                     let c = at_coord(&coord, &maze);
                     if c.is_lowercase() || c.is_uppercase() || c == '@' { Some((c, coord))} else { None }
                 }).collect::<Vec<(char, Coord)>>()
@@ -94,65 +99,9 @@ fn keys(maze: &Vec<String>) -> HashMap<char, Coord> {
     walkables(&maze).into_iter().filter(|(ch, coord)| ch.is_lowercase()).collect()
 }
 
-fn path_to(start: &Coord, goal: &Coord, maze : &Vec<String>) -> Vec<Coord> {
-    let max : usize = 999999999999999999;
-    // build up distances in different subsets, based on what key is already collected
-    let mut dist : HashMap<Coord, usize> = HashMap::new();
-    let mut prev : HashMap<Coord, Coord> = HashMap::new();
-    let mut heap : BinaryHeap<State> = BinaryHeap::new();
-
-
-    dist.insert(start.clone(), 0);
-    heap.push(State { steps: 0, position: start.clone() /*, collected_keys: vec![] */ });
-
-    let mut last = Some(0);
-    //println!("before loop");
-    while let Some(State { steps: steps, position /*, collected_keys */ }) = heap.pop() {
-        let char_at = at_coord(&position, &maze);
-        if char_at == '#' {
-            prev.remove(&position);
-            continue;
-        }
-        // Skip if whe have a better way with the same key combination
-        if steps > *dist.get(&position).unwrap_or(&max) { continue; }
-
-        if position == *goal {
-            println!("found goal!");
-            break; }
-        // Check all neighbors from the current cursor
-        for next_position in position.around() {
-            let next = State { steps: steps + 1, position: next_position.clone()/*, collected_keys: collected_keys.clone()*/};
-            if next.steps < *dist.get(&next_position).unwrap_or(&max) {
-                println!("updating position prev {:?} {:?}", next_position, position);
-                dist.insert(next_position.clone(), next.steps);
-                prev.insert(next_position.clone(), position.clone());
-                heap.push(next);
-            }
-        }
-    }
-
-    let mut path : Vec<Coord> = vec![];
-    path.push(goal.clone());
-    let mut csr = goal.clone();
-    let mut i = 0;
-    while let Some(path_part) = prev.get(&csr) {
-        csr = path_part.clone();
-        println!("{:?}", csr);
-        path.push(path_part.clone());
-
-        if i == 20 {
-            break;
-        }
-        i += 1;
-    }
-    path.reverse();
-
-    path
-}
-
-fn shortest_path_all_keys(maze: &Vec<String>) -> usize {/*
+fn distance(start: &Coord, goal: &Coord, maze: &Vec<String>) -> usize {
     let mut keys = keys(&maze);
-    let start = keys.remove(&'@').unwrap();
+    let start = walkables(maze).remove(&'@').unwrap();
     println!("{:?}", start);
     println!("{:?}", keys);
 
@@ -162,34 +111,101 @@ fn shortest_path_all_keys(maze: &Vec<String>) -> usize {/*
     let mut prev : HashMap<Coord, Coord> = HashMap::new();
     let mut heap : BinaryHeap<State> = BinaryHeap::new();
 
-    heap.push(State { steps: 0, position: start.clone(), collected_keys: vec![] });
+    heap.push(State { steps: 0, position: start.clone()});
+    dist.insert(start.clone(), 0);
 
-    let mut last = Some(0);
-    //println!("before loop");
-    while let Some(State { steps: steps, position, collected_keys: prev_collected_key }) = heap.pop() {
-        //println!("looking at {:?}, {:?}, {:?}", position, prev_collected_key, steps);
+    let mut res = 0;
+    while let Some(State { steps: steps, position: initial_position}) = heap.pop() {
+        let mut position = initial_position.clone();
+        //println!("looking at: {:?}", position);
 
-        let mut collected_keys = prev_collected_key;
-        let char_at = at_coord(&position, &maze);
-        if char_at == '#' {
-            continue;
-        }
         // Skip if whe have a better way with the same key combination
-        if steps > dist[&position] { continue; }
+        if steps > *dist.get(&position).unwrap_or(&max) { continue; }
+
+        if position.collected_keys.len() == keys.len() {
+            res = dist[&initial_position];
+            break;
+        }
 
         // Check all neighbors from the current cursor
-        for next_position in position.around() {
-            let next = State { steps: steps + 1, position: next_position.clone(), collected_keys: collected_keys.clone()};
-            if next.steps < *dist.get(&next_position).unwrap_or(&max) {
-                dist.insert(next_position.clone(), next.steps);
-                prev.insert(next_position.clone(), position.clone());
+        for neighbour_coord in position.around() {
+            let char_at = at_coord(&neighbour_coord, &maze);
+            if char_at == '#' {
+                continue;
+            }
+            let neighbour = if char_at.is_lowercase() && !neighbour_coord.collected_keys.contains(&char_at) {
+                Coord {collected_keys: [neighbour_coord.collected_keys, vec![char_at]].concat(), ..neighbour_coord}
+            } else { neighbour_coord };
+
+            if char_at.is_uppercase() && !neighbour.collected_keys.contains(&char_at.to_lowercase().nth(0).unwrap()){
+                continue; // cannot pass through door yet
+            }
+
+            let mut next = State { steps: steps + 1, position: neighbour.clone()};
+            if next.steps < *dist.get(&neighbour).unwrap_or(&max) {
+                dist.insert(neighbour.clone(), next.steps);
+                prev.insert(neighbour.clone(), position.clone());
                 heap.push(next);
             }
         }
     }
 
-    last.unwrap()*/
-    0
+    res
+}
+
+
+fn shortest_path_all_keys(maze: &Vec<String>) -> usize {
+    let mut keys = keys(&maze);
+    let start = walkables(maze).remove(&'@').unwrap();
+    println!("{:?}", start);
+    println!("{:?}", keys);
+
+    let max : usize = 999999999999999999;
+    // build up distances in different subsets, based on what key is already collected
+    let mut dist : HashMap<Coord, usize> = HashMap::new();
+    let mut prev : HashMap<Coord, Coord> = HashMap::new();
+    let mut heap : BinaryHeap<State> = BinaryHeap::new();
+
+    heap.push(State { steps: 0, position: start.clone()});
+    dist.insert(start.clone(), 0);
+
+    let mut res = 0;
+    while let Some(State { steps: steps, position: initial_position}) = heap.pop() {
+        let mut position = initial_position.clone();
+        //println!("looking at: {:?}", position);
+
+        // Skip if whe have a better way with the same key combination
+        if steps > *dist.get(&position).unwrap_or(&max) { continue; }
+
+        if position.collected_keys.len() == keys.len() {
+            res = dist[&initial_position];
+            break;
+        }
+
+        // Check all neighbors from the current cursor
+        for neighbour_coord in position.around() {
+            let char_at = at_coord(&neighbour_coord, &maze);
+            if char_at == '#' {
+                continue;
+            }
+            let neighbour = if char_at.is_lowercase() && !neighbour_coord.collected_keys.contains(&char_at) {
+                Coord {collected_keys: [neighbour_coord.collected_keys, vec![char_at]].concat(), ..neighbour_coord}
+            } else { neighbour_coord };
+
+            if char_at.is_uppercase() && !neighbour.collected_keys.contains(&char_at.to_lowercase().nth(0).unwrap()){
+                continue; // cannot pass through door yet
+            }
+
+            let mut next = State { steps: steps + 1, position: neighbour.clone()};
+            if next.steps < *dist.get(&neighbour).unwrap_or(&max) {
+                dist.insert(neighbour.clone(), next.steps);
+                prev.insert(neighbour.clone(), position.clone());
+                heap.push(next);
+            }
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -204,25 +220,9 @@ mod test {
     }*/
 
     #[test]
-    fn test_something() {
-        let mut ex =
-"########################
-#f.D.E.e.C.b.A.@.a.B.c.#
-######################.#
-#d.....................#
-########################";
-
-        let mut maze = ex.split('\n').map(String::from).collect();
-        let start = walkables(&maze)[&'@'].clone();
-        let end = walkables(&maze)[&'f'].clone();
-        //println!("{:?}", );
-        assert_eq!(path_to(&start, &end, &maze), vec![]);
-    }
-
-    #[test]
     fn test() {
         let mut ex =
-"########################
+            "########################
 #f.D.E.e.C.b.A.@.a.B.c.#
 ######################.#
 #d.....................#
