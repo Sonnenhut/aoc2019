07aba278d070413eacf08c08d74adc3ac75f6aeb
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-07-03 17:26:53 +0200

    Day 24: Part 1

--- src/bin/day24.rs
+++ src/bin/day24.rs
@@ -5,7 +5,6 @@ use std::fmt::DebugTuple;
 use std::iter;
 use std::ops::{RemAssign, Sub};
 use std::path::Iter;
-use std::slice::SliceIndex;
 use std::sync::mpsc::{Receiver, Sender, SyncSender};
 use std::thread::Thread;
 use std::time::Duration;
@@ -16,7 +15,7 @@ use aoc2019::read_lines;
 
 fn main() {
     let bugs = parse_bugs(read_lines(24));
-    println!("pt1: {}", pt1(&bugs)); // 23253622 too low
+    println!("pt1: {}", pt1(&bugs));
 }
 
 fn pt1(bugs: &Vec<Vec<bool>>) -> usize {
@@ -70,44 +69,58 @@ fn parse_bugs(lines: Vec<String>) -> Vec<Vec<bool>>{
     lines.iter().map(|l| l.chars().map(|c| c == '#').collect()).collect()
 }
 
+/*
+fn evolve2(bugs: HashSet<Tile>) -> HashSet<Tile> {
+
+}
+
+ */
+
 fn around(tile: Tile) -> HashSet<Tile> {
-    let (row, col, depth) = tile;
+    let Tile(row, col, depth) = tile;
     let mut res = HashSet::new();
     // -- tiles touching center
     if row == 2 && col == 1 { // left of center
-        0..5.iter().map(|row_adj| Tile(row_adj,0,depth - 1)).for_each(|t| res.insert(t))
+        (0..5).map(|row_adj| Tile(row_adj,0,depth - 1)).for_each(|t| {res.insert(t);});
     }
     if row == 2 && col == 3 { // right of center
-        0..5.iter().map(|row_adj| Tile(row_adj,4,Depth - 1)).for_each(|t| res.insert(t))
+        (0..5).map(|row_adj| Tile(row_adj,4,depth - 1)).for_each(|t| {res.insert(t);});
     }
     if row == 1 && col == 2 { // above center
-        0..5.iter().map(|col_adj| Tile(0,col_adj,Depth - 1)).for_each(|t| res.insert(t))
+        (0..5).map(|col_adj| Tile(0,col_adj,depth - 1)).for_each(|t| {res.insert(t);});
     }
     if row == 3 && col == 2 { // below center
-        0..5.iter().map(|col_adj| Tile(4,col_adj,Depth - 1)).for_each(|t| res.insert(t))
+        (0..5).map(|col_adj| Tile(4,col_adj,depth - 1)).for_each(|t| {res.insert(t);});
     }
 
     // on outer border (depth +1)
     // upper edge
+    println!("#{:?}", res);
     if row == 0 { res.insert(Tile(1,2,depth+1)); }
+    println!("{:?}", res);
     // lower edge
     if row == 4 { res.insert(Tile(3,2,depth+1)); }
+    println!("{:?}", res);
     // left edge
     if col == 0 { res.insert(Tile(2,1,depth+1)); }
+    println!("{:?}", res);
     // right edge
-    if col == 0 { res.insert(Tile(2,3,depth+1)); }
+    if col == 4 { res.insert(Tile(2,3,depth+1)); }
+    println!("{:?}", res);
 
-    if let Some(row_adj) = row.checked_sub(1) { res.insert(Tile(row_adj,col,depth)) }
-    if let Some(row_adj) = row.checked_add(1) { res.insert(Tile(row_adj,col,depth)) }
-    if let Some(col_adj) = col.checked_sub(1) { res.insert(Tile(row,col_adj,depth)) }
-    if let Some(col_adj) = col.checked_add(1) { res.insert(Tile(row,col_adj,depth)) }
+    if row != 0 { res.insert(Tile(row - 1,col,depth)); }
+    if col != 0 { res.insert(Tile(row,col - 1,depth)); }
+    if row != 4 { res.insert(Tile(row + 1,col,depth)); }
+    if col != 4 { res.insert(Tile(row,col + 1,depth)); }
 
     res.remove(&Tile(2,2,depth)); // center element does not exist..
     res
 }
 
 
+#[derive(Debug, PartialEq, Clone, Copy, Hash, Eq)]
 struct Tile(usize,usize, isize);
+#[derive(Debug, PartialEq, Clone, Copy, Hash, Eq)]
 struct Pos(usize, usize);
 
 #[cfg(test)]
@@ -119,8 +132,8 @@ mod test {
 
     #[test]
     fn regression() {
-        let mem: Vec<i64> = read_lines(23)[0].split(',').map(|s| s.parse().unwrap()).collect();
-        //assert_eq!(pt1(&mem), 17949);
+        let bugs = parse_bugs(read_lines(24));
+        assert_eq!(pt1(&bugs), 24662545);
         //assert_eq!(pt2(&mem), 12326);
     }
 
@@ -200,6 +213,22 @@ mod test {
         assert_eq!(is_bug(1,2, &bugs), true);
         assert_eq!(is_bug(2,3, &bugs), true);
         assert_eq!(is_bug(0,1, &bugs), false);
+    }
+
+    #[test]
+    fn around_test() {
+        // on same lvl
+        assert_eq!(around(Tile(1,1,0)),vec![Tile(2,1,0),Tile(0,1,0),Tile(1,0,0),Tile(1,2,0)].into_iter().collect());
+        // Tile 19 has four adjacent tiles: 14, 18, 20, and 24.
+        assert_eq!(around(Tile(3,3,1)),vec![Tile(2,3,1),Tile(3,2,1),Tile(3,4,1),Tile(4,3,1)].into_iter().collect());
+        // Tile D has four adjacent tiles: 8, C, E, and I.
+        assert_eq!(around(Tile(0,3,0)),vec![Tile(1,2,1),Tile(0,2,0),Tile(0,4,0),Tile(1,3,0)].into_iter().collect());
+        // Tile E has four adjacent tiles: 8, D, 14, and J.
+        assert_eq!(around(Tile(0,4,0)), vec![Tile(1,2,1),Tile(2,3,1),Tile(0,3,0),Tile(1,4,0)].into_iter().collect());
+        // Tile 14 has eight adjacent tiles: 9, E, J, O, T, Y, 15, and 19.
+        assert_eq!(around(Tile(2,3,1)), vec![Tile(0,4,0),Tile(1,4,0),Tile(2,4,0),Tile(3,4,0),Tile(4,4,0),Tile(3,3,1),Tile(1,3,1),Tile(2,4,1)].into_iter().collect());
+        //Tile N has eight adjacent tiles: I, O, S, and five tiles within the sub-grid marked ?.
+        assert_eq!(around(Tile(2,3,0)), vec![Tile(0,4,-1),Tile(1,4,-1),Tile(2,4,-1),Tile(3,4,-1),Tile(4,4,-1),Tile(3,3,0),Tile(1,3,0),Tile(2,4,0)].into_iter().collect());
     }
 
 }
